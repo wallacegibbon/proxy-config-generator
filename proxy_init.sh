26c0dfa48e19e8769472e52cf836d844bab74c4b
Wallace Gibbon <wallacegibbon@aliyun.com>
2026-08-05 15:27:43 +0800

    feat: expand no_proxy to include private and loopback network ranges

--- proxy_init.sh
+++ proxy_init.sh
@@ -3,11 +3,11 @@ proxyon() {
 	export all_proxy=socks5://127.0.0.1:7891
 	export http_proxy=http://127.0.0.1:7890
 	export https_proxy=http://127.0.0.1:7890
-	export no_proxy=127.0.0.1,localhost
+	export no_proxy="localhost,127.0.0.0/8,::1,192.168.0.0/16,172.16.0.0/12,10.0.0.0/8"
 	export ALL_PROXY=socks5://127.0.0.1:7891
 	export HTTP_PROXY=http://127.0.0.1:7890
 	export HTTPS_PROXY=http://127.0.0.1:7890
-	export NO_PROXY=127.0.0.1,localhost
+	export NO_PROXY="localhost,127.0.0.0/8,::1,192.168.0.0/16,172.16.0.0/12,10.0.0.0/8"
 	printf "\033[32m[√] Proxy env set up!\033[0m\n"
 }
 
